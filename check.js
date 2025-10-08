eaa854e8f5084edcd09c8f62f394089ee624c798
Peam <peammy2010@hotmail.com>
2025-10-08 20:46:56 +0700

    refactor: replace image validation with duplicate answer detection in quiz checker

--- check.js
+++ check.js
@@ -1,10 +1,18 @@
 import quiz from "./quiz.json";
+// for (const item of quiz.items) {
+//   try {
+//     const res = await fetch(item.src);
+//     if (res.status != 200) console.log(item.answer);
+//   } catch (error) {
+//     console.log(item.answer);
+//   }
+// }
 
+// find dup
+const a = new Set();
 for (const item of quiz.items) {
-  try {
-    const res = await fetch(item.src);
-    if (res.status != 200) console.log(item.answer);
-  } catch (error) {
+  if (a.has(item.answer)) {
     console.log(item.answer);
   }
+  a.add(item.answer);
 }
